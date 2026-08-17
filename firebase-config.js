b0509fa062c5c3f002d3be535a15646bc3eab264
azizijohan92 <mohdazizijohan@gmail.com>
2026-08-17 14:08:30 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,16 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+    getAuth
+} from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBPEGVpxl15Akx9VBt-AT9bABvO2s1M3_w",
   authDomain: "ninjamath-b88d2.firebaseapp.com",
@@ -7,3 +20,8 @@ const firebaseConfig = {
   messagingSenderId: "276177438351",
   appId: "1:276177438351:web:a4cb3ec4b7caac126d12fa"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
