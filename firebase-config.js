e3d35e2fa3f7a9e0a16dcb680ea8e6276edefa2f
azizijohan92 <mohdazizijohan@gmail.com>
2026-08-17 14:00:53 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBPEGVpxl15Akx9VBt-AT9bABvO2s1M3_w",
+  authDomain: "ninjamath-b88d2.firebaseapp.com",
+  databaseURL: "https://ninjamath-b88d2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ninjamath-b88d2",
+  storageBucket: "ninjamath-b88d2.firebasestorage.app",
+  messagingSenderId: "276177438351",
+  appId: "1:276177438351:web:a4cb3ec4b7caac126d12fa"
+};
