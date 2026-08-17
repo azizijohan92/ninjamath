29519a8d52ca7bb4979835b1a27572c87d93d1af
azizijohan92 <mohdazizijohan@gmail.com>
2026-08-17 14:36:12 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,27 +1,58 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+    initializeApp
+}
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+
 
 import {
     getAuth
-} from
-"https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+}
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
 
 import {
     getFirestore
-} from
-"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+}
+from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPEGVpxl15Akx9VBt-AT9bABvO2s1M3_w",
-  authDomain: "ninjamath-b88d2.firebaseapp.com",
-  databaseURL: "https://ninjamath-b88d2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ninjamath-b88d2",
-  storageBucket: "ninjamath-b88d2.firebasestorage.app",
-  messagingSenderId: "276177438351",
-  appId: "1:276177438351:web:a4cb3ec4b7caac126d12fa"
+
+    apiKey:
+        "AIzaSyBPEGVpxl15Akx9VBt-AT9bABvO2s1M3_w",
+
+    authDomain:
+        "ninjamath-b88d2.firebaseapp.com",
+
+    databaseURL:
+        "https://ninjamath-b88d2-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+    projectId:
+        "ninjamath-b88d2",
+
+    storageBucket:
+        "ninjamath-b88d2.firebasestorage.app",
+
+    messagingSenderId:
+        "276177438351",
+
+    appId:
+        "1:276177438351:web:a4cb3ec4b7caac126d12fa"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+const app =
+    initializeApp(
+        firebaseConfig
+    );
+
+
+export const auth =
+    getAuth(app);
+
+
+export const db =
+    getFirestore(app);
